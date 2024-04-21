4c98d3cc81dd52240f8a2219923864680451d846
Vadimir Avvakumov <Birmengeem@bk.ru>
2024-04-21 15:42:36 +0300

    Первая программа

--- seminar1/Program.cs
+++ seminar1/Program.cs
@@ -11,7 +11,9 @@
 // string num1str = Console.ReadLine();
 // int num = Convert.ToInt32(num1str);
 
+Console.Write("Введите первое число");
 int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число");
 int num2 = Convert.ToInt32(Console.ReadLine());
 
 if(num1 == num2 * num2)
