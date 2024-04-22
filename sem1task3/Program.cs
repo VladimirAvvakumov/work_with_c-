87140a07e9e149de5a0b312f309d070fd33e857c
Vadimir Avvakumov <Birmengeem@bk.ru>
2024-04-22 19:53:36 +0300

    прога дописана

--- sem1task3/Program.cs
+++ sem1task3/Program.cs
@@ -8,8 +8,17 @@
 Console.WriteLine("введите целое трехзначное число");
 int num = Convert.ToInt32(Console.ReadLine());
 
-int FirstDigit = num / 100;
-int LastDigit = num % 10;
-int sum = FirstDigit + LastDigit;
 
-Console.WriteLine($"сумма первой и последней цифры = {sum}");
+if (num > 99 && num < 1000)
+{
+
+    int FirstDigit = num / 100;
+    int LastDigit = num % 10;
+    int sum = FirstDigit + LastDigit;
+
+    Console.WriteLine($"сумма первой и последней цифры = {sum}");
+}
+else
+{
+    Console.WriteLine("неправильное число");
+}
