27b34f5e19977fc0f61ec257d43aa352df416d66
Vadimir Avvakumov <Birmengeem@bk.ru>
2024-04-22 19:46:22 +0300

    исправление ошибок

--- seminar1/Program.cs
+++ seminar1/Program.cs
@@ -11,6 +11,8 @@
 // string num1str = Console.ReadLine();
 // int num = Convert.ToInt32(num1str);
 
+
+
 Console.Write("Введите первое число");
 int num1 = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число");
