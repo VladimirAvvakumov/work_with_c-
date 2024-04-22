0a935629609e7b683d1852327616f00c9eda3245
Vadimir Avvakumov <Birmengeem@bk.ru>
2024-04-22 19:42:25 +0300

    Третья прога проработана

--- sem1task3/Program.cs
+++ sem1task3/Program.cs
@@ -1,4 +1,15 @@
-﻿
-Console.WriteLine();
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное целое число и на выходе показывает
+// сумму первой и последней цифры этого числа.
+// 456 => 10
+// 782 => 9
+// 918 => 17
 
+Console.WriteLine("введите целое трехзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
 
+int FirstDigit = num / 100;
+int LastDigit = num % 10;
+int sum = FirstDigit + LastDigit;
+
+Console.WriteLine($"сумма первой и последней цифры = {sum}");
