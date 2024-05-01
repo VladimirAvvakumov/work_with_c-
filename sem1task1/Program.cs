407736be433817d66c2aa7a74a1c5e15d082a486
Vadimir Avvakumov <Birmengeem@bk.ru>
2024-05-01 17:56:18 +0300

    new seminar

--- sem1task1/Program.cs
+++ sem1task1/Program.cs
@@ -13,9 +13,12 @@
 
 
 
-Console.Write("Введите первое число");
+Console.WriteLine("Введите первое число");
+
 int num1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число");
+
+Console.WriteLine("Введите второе число");
+
 int num2 = Convert.ToInt32(Console.ReadLine());
 
 if(num1 == num2 * num2)
