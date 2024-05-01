a958440712879069224cef4d5b510beb27e7e488
Vadimir Avvakumov <Birmengeem@bk.ru>
2024-05-01 18:53:07 +0300

    работа с преподователем

--- sem3task1/Program.cs
+++ sem3task1/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайтемассив.Напишите программу,
+// которая определяет,присутствует ли заданное число в массиве.
+// Программа должна выдать ответ:Да/Нет.
+// Примеры
+// [1 3 4 19 3], 8 => Нет
+// [-4 3 4 1], 3 => Да
+
+
+// int[] array1 = new int[5];
+// int[] array2 = new int[5]{1,3,4,19,3};
+// int[] array3 = new int[] {1,3,4,19,3};
+
+int[] array = { 1, 3, 4, 19, 3 };
+
+int num = 8;
+bool isExistNum = false;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == num)
+    {
+        isExistNum = true;
+        break;
+    }
+}
+// string result = isExistNum ? "да" : "нет"; теринарный оператор "Работает с тремя переменными" ниже упрощенная версия
+Console.WriteLine(isExistNum ? "да" : "нет");
