78158b6582fa1abf9aca8e8e6ad51ffd017ea1a6
Vadimir Avvakumov <Birmengeem@bk.ru>
2024-05-12 16:28:13 +0300

    functions2

--- Functions1/Program.cs
+++ Functions1/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿//Вычислить значение формулы (a*b)/(C+d)
+
+double CalculateFormula(int a, int b, int c, int d)
+{
+    int numenator = a * b;
+    int denumenator = c + d;
+    double result = numenator / denumenator;
+    return result;
+}
+double result = CalculateFormula(3, 2, 3, 3);
+
+Console.WriteLine(result);
+
+
