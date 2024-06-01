c497f516d55aba835f54b5e9ae8eb8e143b66f03
Vadimir Avvakumov <Birmengeem@bk.ru>
2024-06-01 14:48:37 +0300

    begining recursions HomeWork

--- Recursion4/Program.cs
+++ Recursion4/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Считать строку с консоли, содержащую латинские
+// буквы. Вывести на экран согласные буквы этой
+// строки.
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+// Пример
+// “hello” => h l l
+// “World” => W r l d
+// “Hello world!” => H l l w r l d
+
+static void CheckPrintConsonats(string txt, int index)
+{
+string vowels = "aeiouy";
+if (!vowels.Contains(txt[index]) ) //&& char.IsAsciiLetter(txt[index]) этот метод проверки отсутствует.
+{
+Console.Write($"{txt[index]}");
+}
+}
+
+void PrintConsonants(string txt, int index = 0)
+{
+if (index == txt.Length)
+{
+return;
+}
+CheckPrintConsonats(txt, index);
+PrintConsonants(txt, index + 1);
+}
+
+Console.WriteLine("введите строку содержащую латинские буквы: ");
+string text = Console.ReadLine();
+Console.WriteLine();
+PrintConsonants(text);
+
+
+
+
