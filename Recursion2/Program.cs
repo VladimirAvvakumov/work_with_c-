adea9d6b4e91cdcb1dbf4cd53066ce986665a00b
Vadimir Avvakumov <Birmengeem@bk.ru>
2024-06-01 14:49:29 +0300

    begining recursions HomeWork

--- Recursion2/Program.cs
+++ Recursion2/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. Напишите программу, которая выведет все
+// натуральные числа в промежутке от 1 до N.
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+// Пример
+// N=5 => 1 2 3 4 5
+
+void PrintNaturalNumbers(int num)
+{
+    if(num == 0) return;
+    PrintNaturalNumbers(num -1);// функция вызывает сама себя(рекурсия)
+    Console.Write($"{num} ");// если переставить на две строчки выше то выведеться в обратном порядке. 
+}
+
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+PrintNaturalNumbers(number);
+
+
+// int Factorial(int n)
+// {
+// if(n == 1) return 1;
+// return n * Factorial(n - 1);
+// }
+
+// Console.WriteLine(Factorial(5)); // 120
+
+
---
+++ stringHomeWork1/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задайте двумерный массив символов (тип char [,]).
+//  Создать строку из символов этого массива.
+
+
+
+
+char[,] array = new char[,]
+{
+    {'H','e','l','l','o','W','o','r','l','d'},
+    {'W','o','r','l','d','Y','e','l','l','o'}
+};
+string Onestring(char[,] array)
+{
+    string result = string.Empty;
+    for (int i = 0; i < array.GetLength(0); i++)// проходим циклом по всему слову
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            result = result + array[i, j];
+        }
+
+    }
+    return result;
+}
+
+string arr = Onestring(array);
+Console.WriteLine(arr);
